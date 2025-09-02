b2739eeb0d2dd162d160f25e3aa0e295886780ea
Amaury TISSOT <tissot.amaury@gmail.com>
2025-09-02 11:46:52 +0200

    feat: add camera page and photos gallery pages

--- app/(tabs)/photos.jsx
+++ app/(tabs)/photos.jsx
@@ -1,12 +1,51 @@
-import React from "react";
-import { Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import React, { useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet, View } from "react-native";
 
-const photos = () => {
+export default function Photos() {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        loadPhotos();
+    }, []);
+
+    const loadPhotos = async () => {
+        try {
+            const files = await FileSystem.readDirectoryAsync(
+                FileSystem.documentDirectory
+            );
+            const imageFiles = files
+                .filter((file) => file.endsWith(".jpg"))
+                .map((file) => FileSystem.documentDirectory + file);
+
+            setPhotos(imageFiles);
+        } catch (err) {
+            console.log("Erreur lecture dossier photos:", err);
+        }
+    };
+
     return (
-        <View>
-            <Text>photos</Text>
+        <View style={styles.container}>
+            <FlatList
+                data={photos}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <Image source={{ uri: item }} style={styles.photo} />
+                )}
+            />
         </View>
     );
-};
+}
 
-export default photos;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    photo: {
+        width: 200,
+        height: 200,
+        margin: 5,
+        borderRadius: 10,
+    },
+});
