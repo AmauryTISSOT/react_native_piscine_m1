c3fad77fe8e240d5985cddd090d3640b2f5305c6
Amaury TISSOT <tissot.amaury@gmail.com>
2025-09-04 09:27:05 +0200

    feat: add gps location and refactor camera page

--- app/(tabs)/camera.jsx
+++ app/(tabs)/camera.jsx
@@ -1,19 +1,123 @@
-import TakePhoto from "@/components/TakePhoto";
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import PhotoPreviewSection from "@/components/PhotoPreviewSection";
+import { saveAndUploadPhoto } from "@/services/photoService";
+import { AntDesign } from "@expo/vector-icons";
+import { useIsFocused } from "@react-navigation/native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useRef, useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const Camera = () => {
+export default function Camera() {
+    const isFocused = useIsFocused();
+    const [facing, setFacing] = useState("back");
+    const [permission, requestPermission] = useCameraPermissions();
+    const [isPreview, setIsPreview] = useState(false);
+    const [photo, setPhoto] = useState(null);
+    const cameraRef = useRef(null);
+
+    if (!permission) {
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>
+                    Nous avons besoin de votre permission pour montrer la caméra
+                </Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    function toggleCameraFacing() {
+        setFacing((current) => (current === "back" ? "front" : "back"));
+    }
+
+    const handleTakePhoto = async () => {
+        if (cameraRef.current) {
+            const takedPhoto = await cameraRef.current.takePictureAsync({
+                quality: 1,
+                base64: true,
+                exif: false,
+            });
+
+            setPhoto(takedPhoto);
+            setIsPreview(true);
+        }
+    };
+
+    const handleRetakePhoto = () => {
+        setPhoto(null);
+        setIsPreview(false);
+    };
+
+    const handleSavePhoto = async () => {
+        if (photo?.base64) {
+            await saveAndUploadPhoto(photo.base64);
+            setPhoto(null);
+            setIsPreview(false);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <TakePhoto />
+            {photo && isPreview ? (
+                <PhotoPreviewSection
+                    photo={photo}
+                    handleRetakePhoto={handleRetakePhoto}
+                    handleSavePhoto={handleSavePhoto}
+                />
+            ) : isFocused ? (
+                <CameraView
+                    style={styles.camera}
+                    facing={facing}
+                    ref={cameraRef}
+                >
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={toggleCameraFacing}
+                        >
+                            <AntDesign name="retweet" size={44} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={handleTakePhoto}
+                        >
+                            <AntDesign name="camera" size={44} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                </CameraView>
+            ) : null}
         </View>
     );
-};
-
-export default Camera;
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: "center",
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "transparent",
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: "flex-end",
+        alignItems: "center",
+        marginHorizontal: 10,
+        backgroundColor: "gray",
+        borderRadius: 10,
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white",
     },
 });
